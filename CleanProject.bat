46b16abee2e71d0d9c370826284491039834c68d
martinpiper <martin.piper@gmail.com>
2013-11-03 17:22:22 +0800

    More tidying

--- CleanProject.bat
+++ CleanProject.bat
@@ -1,6 +1,8 @@
 del *.pdb *.ncb *.lib *.obj *.pch *.sbr *.bsc *.idb *.ilk *.plg *.dll BuildLog.htm *.pcc *.dep *.res *.xex /s
-del *.xdb *.map *.nlb UpgradeLog*.xml UpgradeReport*.* *.pe /s
+del *.xdb *.map *.nlb UpgradeLog*.xml UpgradeReport*.* *.pe *.sdf /s
 del *.bnd /s
 
 rmdir /s /q Debug
 rmdir /s /q Release
+
+del *.lbl *.map *.prg /s
