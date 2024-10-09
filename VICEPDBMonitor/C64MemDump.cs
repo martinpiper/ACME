07a08566628c60364f909fe4a26ac9bdcf4e84a2
Martin Piper <martin.piper@gmail.com>
2024-10-09 21:38:06 +0800

    Update C64MemDump.cs

--- VICEPDBMonitor/C64MemDump.cs
+++ VICEPDBMonitor/C64MemDump.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Threading;
+using System.Globalization;
 
 namespace VICEPDBMonitor
 {
@@ -60,5 +61,38 @@ namespace VICEPDBMonitor
         {
             Buffer.BlockCopy(reply, 0, mMemoryC64, 0, reply.Length);
         }
+
+        private void ParseMemory(string theReply)
+        {
+            string[] split = theReply.Split('\n');
+            int index = 0;
+            while (index < split.Length)
+            {
+                string line = split[index++];
+                //>C:0010  4c 39 32 39  4c 69 8b ad  ca dd e4 dd  ca ad 8b 69   L929Li.........i
+                line = line.ToLower();
+                if (!line.StartsWith(">c:"))
+                {
+                    continue;
+                }
+                try
+                {
+                    string tAddr = line.Substring(3, 4);
+                    int theAddr = int.Parse(tAddr, NumberStyles.HexNumber);
+                    string remain = line.Substring(9);
+                    string[] splits2 = remain.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    int index2 = 0;
+                    while (index2 < splits2.Length - 1)
+                    {
+                        mMemoryC64[theAddr + index2] = (byte)int.Parse(splits2[index2], NumberStyles.HexNumber); ;
+                        index2++;
+                    }
+                }
+                catch (System.Exception)
+                {
+                }
+            }
+        }
+
     }
 }
